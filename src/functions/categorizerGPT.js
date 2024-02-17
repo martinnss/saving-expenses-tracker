f832168879d6dec5999e4eb4db9e5a1f8b1c1b50
martinnss <martin.olivarest@utem.cl>
2024-02-17 18:21:42 -0300

    major uupdate, backend created and deployed

--- src/functions/categorizerGPT.js
+++ src/functions/categorizerGPT.js
@@ -2,10 +2,7 @@
 import OpenAI from "openai";
 import apiKey from "./keys";
 
-const openai = new OpenAI({
-	apiKey: apiKey,
-	dangerouslyAllowBrowser: true
-});
+
 
 let count = 0
 
@@ -33,81 +30,42 @@ async function categorizerGPT(listOfObjects) {
   const textOfSellers = descriptions.join(', ');
 
 
+  try {
+    // Realizar el llamado a la ruta de Express
+    const response = await fetch('https://saving-backend-3ngcl3pz5-martinnss.vercel.app/get-category', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ textOfSellers }),
+    });
+
+    // Verificar si la respuesta es exitosa (código 200)
+    if (response.ok) {
+        const completion = await response.json();
+        // Manejar la data según sea necesario
+        const gptOutput = completion.choices[0].message.content 
+        const jsonString = gptOutput
+      
+        // Parse JSON
+        const jsonOutput = JSON.parse(jsonString);
 
 
-  
-  const completion = await openai.chat.completions.create({
-    messages: [{"role": "system", "content": 'Identify a category associated with a given seller name. If the provided name appears to be a personal name, classify it as a "small business." Generate ajson ready to parse in javascript containing "seller" : "category" for every single seller'},
-        {"role": "user", "content": textOfSellers}],
-    model: "gpt-3.5-turbo-1106",
-    response_format: { type: "json_object" },
-	  temperature: 0.3
-  });
+        // hacer un joint porporla desc y las categories como sale abajo
 
-  const gptOutput = completion.choices[0].message.content 
-  const jsonString = gptOutput
-/*
+        const transactionsWithCategories = addCategoriesToTransactions(listOfSellers, jsonOutput);
 
-  
-  const jsonString=`
-  {
-    "JUMBO COSTANERA CEN SANTIAGO": "Supermarket",
-    "HDI SEGUROS SA SANTIAGO": "Insurance",
-    "MERPAGO*SOCIEDAD GA PROVIDENCIA": "Financial Services",
-    "FARMACIA NETFARMA SANTIAGO": "Pharmacy",
-    "MERPAGO*ENTELPCSMOV LAS CONDES": "Telecommunications",
-    "CLARO 11001SANTIAG": "Telecommunications",
-    "MERPAGO*TRANSVIPWEB SANTIAGO": "Transportation",
-    "DECATHLON COSTAN CE TASA INT. 2,98%": "Sporting Goods",
-    "MP *NUNEZ Y ZUN TASA INT. 2,98%": "Retail",
-    "ADIDAS ORIGINALS OE TASA INT. 2,96%": "Sporting Goods",
-    "CLINICA QUIROPRACTI TASA INT. 0,00%": "Healthcare",
-    "UNIRED CL ENEL TASA INT. 0,00%": "Utilities",
-    "TERMAS DEL SOL TASA INT. 0,00%": "Hospitality",
-    "C. VERDE A. VARAS 9 TASA INT. 2,88%": "Restaurant",
-    "CAFE Y RESTAURANT C TASA INT. 2,88%": "Restaurant",
-    "HOTEL VERSALLES TASA INT. 0,00%": "Hospitality",
-    "LOMITOS PORKY SPA TASA INT. 2,88%": "Restaurant",
-    "UNIRED CL VESPUCIO TASA INT. 0,00%": "Utilities",
-    "AUTOPISTA CENTRAL TASA INT. 0,00%": "Transportation",
-    "STA ISABEL AV OSSA TASA INT. 0,00%": "Retail",
-    "SIRIO SPA TASA INT. 2,88%": "Beauty and Wellness",
-    "PARIS INTERNET TCOM TASA INT. 2,88%": "Retail",
-    "FALABELLA COSTANERA TASA INT. 0,00%": "Retail",
-    "DJI DRONESTORE COST TASA INT. 2,88%": "Electronics",
-    "SOC COMERCIAL LAS B TASA INT. 0,00%": "Retail",
-    "STA ISABEL JM CARRE TASA INT. 0,00%": "Retail",
-    "AUTOPLANET GRAN AV TASA INT. 2,88%": "Automotive",
-    "SALON UNISEX OSCAR TASA INT. 0,00%": "Beauty and Wellness",
-    "ABUUA FLORES SPA TASA INT. 2,88%": "Florist",
-    "COMERCIAL H Y P LTD TASA INT. 0,00%": "Retail",
-    "COMERCIAL H Y P LTD TASA INT. 2,91%": "Retail",
-    "CAFE DE LOLOL TASA INT. 0,00%": "Restaurant",
-    "COMERCIAL DE PABLO TASA INT. 0,00%": "Retail",
-    "VESPUCIO NORTE TASA INT. 0,00%": "Retail",
-    "AUTOPISTA N VESP SU TASA INT. 0,00%": "Transportation",
-    "SOCIEDAD CONCESIONA TASA INT. 0,00%": "Transportation",
-    "RUTA DEL MAIPO SC S TASA INT. 0,00%": "Transportation",
-    "TELETON TASA INT. 0,00%": "Nonprofit",
-    "SHEIN TASA INT. 2,91%": "Fashion",
-    "ALVAREZ DE SARRATEA TASA INT. 0,00%": "Retail",
-    "PASSARO GONZALEZ LE TASA INT. 2,91%": "Legal Services",
-    "RIPLEY PLAZA OESTE TASA INT. 0,00%": "Retail",
-    "JUMBO EL LLANO TASA INT. 0,00%": "Supermarket"
+
+        return transactionsWithCategories
+    } else {
+        // Manejar errores de respuesta
+        console.error('Error en la respuesta del servidor:', response.status);
+    } 
+  } catch (error) {
+    // Manejar errores de red u otros
+    console.error('Error en la solicitud:', error.message);
   }
-  `*/
-  // Log the extracted JSON string for debugging
-  console.log(jsonString)
-  // Parse JSON
-  const jsonOutput = JSON.parse(jsonString);
-
-
-  // hacer un joint porporla desc y las categories como sale abajo
-
-  const transactionsWithCategories = addCategoriesToTransactions(listOfSellers, jsonOutput);
-
-
- return transactionsWithCategories
 }
+
 
 export default categorizerGPT
--- src/functions/categorizerGPTEmails.js
+++ src/functions/categorizerGPTEmails.js
@@ -1,10 +1,7 @@
 import OpenAI from "openai";
 import apiKey from "./keys";
 
-const openai = new OpenAI({
-	apiKey: apiKey,
-	dangerouslyAllowBrowser: true
-});
+
 
 
 
@@ -30,34 +27,61 @@ async function categorizerGPTEmails(listOfObjects) {
   // Utiliza el método map para obtener un array de todas las descripciones
   const descriptions = listOfSellers.map(objeto => objeto.desc);
   
-
+  console.log(descriptions)
   // Usa el método join para unir las descripciones con comas
-  const textOfSellers = descriptions.join(', ');
+  const textOfSellers = descriptions.join('| ');
 
+  console.log(textOfSellers)
 
+  /*
+  const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_OPENAI_KEY,
+  });
 
   const completion = await openai.chat.completions.create({
     messages: [{"role": "system", "content": 'Identify a category associated with a given seller name. If the provided name appears to be a personal name, classify it as a "small business." Generate ajson ready to parse in javascript containing "seller" : "category" for every single seller'},
         {"role": "user", "content": textOfSellers}],
     model: "gpt-3.5-turbo-1106",
     response_format: { type: "json_object" },
-	  temperature: 0.3
+	  temperature: 0.4
   });
+  */
+  try {
+    // Realizar el llamado a la ruta de Express
+    const response = await fetch('https://saving-backend-3ngcl3pz5-martinnss.vercel.app/get-category', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ textOfSellers }),
+    });
 
-  const gptOutput = completion.choices[0].message.content 
-  const jsonString = gptOutput
-
-  // Log the extracted JSON string for debugging
-  // Parse JSON
-  const jsonOutput = JSON.parse(jsonString);
-
-
-  // hacer un joint porporla desc y las categories como sale abajo
-
-  const transactionsWithCategories = addCategoriesToTransactions(listOfSellers, jsonOutput);
-
-  
-  return transactionsWithCategories
+    // Verificar si la respuesta es exitosa (código 200)
+    if (response.ok) {
+        const completion = await response.json();
+        // Manejar la data según sea necesario
+        const gptOutput = completion.choices[0].message.content 
+        const jsonString = gptOutput
+      
+        // Log the extracted JSON string for debugging
+        // Parse JSON
+        const jsonOutput = JSON.parse(jsonString);
+      
+        console.log(jsonOutput)
+        // hacer un joint porporla desc y las categories como sale abajo
+      
+        const transactionsWithCategories = addCategoriesToTransactions(listOfSellers, jsonOutput);
+      
+        
+        return transactionsWithCategories
+    } else {
+        // Manejar errores de respuesta
+        console.error('Error en la respuesta del servidor:', response.status);
+    } 
+  } catch (error) {
+    // Manejar errores de red u otros
+    console.error('Error en la solicitud:', error.message);
+  }
 }
 
 export default categorizerGPTEmails
